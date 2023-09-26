5bcfce754ecb65ed15d1c7274ba2e3ad435fd7fb
DeathG7n <christariccykid55@gmail.com>
2023-09-26 08:14:58 +0100

    reinvent

--- api.js
+++ api.js
@@ -3,9 +3,7 @@ const DerivAPIBasic = require('@deriv/deriv-api/dist/DerivAPIBasic');
 const connection = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=36807');
 const ta = require('ta.js')
 const express = require('express')
-const axios = require('axios')
-const path = require("path")
-
+const api = new DerivAPIBasic({ connection })
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
@@ -16,17 +14,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-
-
-const PORT = 3000;
-
-const api = new DerivAPIBasic({ connection });
-
-// -----------------------Deployment-------------------------
-
-
-// ----------------------------------------------------------
-
 const app = express();
 
 app.get('/', async(req, res) => {
@@ -34,10 +21,10 @@ app.get('/', async(req, res) => {
 });
 
 let interval
-
+const PORT = 3000;
 const server = app.listen(PORT, () => {
   getTicksHistory()
-  interval = setInterval(()=> getTicksHistory(), 90000)
+  interval = setInterval(()=> getTicksHistory(), 300000)
   console.log(`Server is running on port ${PORT}`);
 });
 
@@ -48,8 +35,11 @@ const io = require("socket.io")(server, {
 })
 
 io.on("connection", (socket) =>{
-    console.log("connected to socket.io")
+  console.log("connected to socket.io")
 })
+
+let breakOfStructure
+let changeOfCharacter
 
 function getTicksRequest(symbol, count){
     const ticks_history_request = {
@@ -57,64 +47,71 @@ function getTicksRequest(symbol, count){
         count: count,
         end: 'latest',
         style: 'candles',
-        granularity: 60,
+        granularity: 300,
     };
     return ticks_history_request
 }
-
+console.log(1)
 const symbol = 'R_75'
 const getTicksHistory = async () => {
-    const date = new Date()
+  try{
     const period_21 = getTicksRequest(symbol, 21)
     const period_50 = getTicksRequest(symbol, 50)
+    const period_51 = getTicksRequest(symbol, 51)
     const candles_21 = await api.ticksHistory(period_21);
     const candles_50 = await api.ticksHistory(period_50);
+    const candles_51 = await api.ticksHistory(period_51);
     const closePrices21 = candles_21?.candles?.map(i => {return i?.close - 39.9991})
     const highPrices = candles_50?.candles?.map(i => {return i?.high - 35.4976})
     const lowPrices = candles_50?.candles?.map(i => {return i?.low - 35.5046})
     const openPrices21 = candles_21?.candles?.map(i => {return i?.open - 35.5046})
     const closePrices50 = candles_50?.candles?.map(i => {return i?.close - 39.9991})
-    const ma21 = ta.sma(closePrices21, closePrices21.length)
-    const ma50 = ta.sma(closePrices50, closePrices50.length)
+    const closePrices51 = candles_51?.candles?.map(i => {return i?.close - 39.9991})
+    const ma21 = ta.ema(closePrices21, closePrices21.length)
+    const ma50 = ta.ema(closePrices50, closePrices50.length)
+    const prevMa50 = ta.ema(closePrices51, closePrices51.length - 1)
     const data = []
-    const isUptrend = ma21 > ma50 ? true : false
+    const isUptrend = ma21[0] > ma50[0] ? true : false
     for (let i = 0; i < highPrices.length; i++) {
         data.push([highPrices[i], lowPrices[i]])
     }
     const fractals = ta.fractals(data)
-    // if (isUptrend){
-    //     if (fractals[47][0] == true){
-    //         const mailOptions = {
-    //             from: 'christariccykid55@gmail.com',
-    //             to: 'meliodasdemonk8ng@gmail.com',
-    //             subject: `Upper Fractal formed at ${data[47][0]}`,
-    //             text: 'Trading Signal'
-    //         };
+    if (isUptrend){
+        if (openPrices21[19] > prevMa50[0] && closePrices21[19] < prevMa50[0]){
+            const mailOptions = {
+                from: 'christariccykid55@gmail.com',
+                to: 'meliodasdemonk8ng@gmail.com',
+                subject: `Market Structure broken at ${prevMa50[0]}`,
+                text: 'Trading Signal'
+            };
               
-    //         transporter.sendMail(mailOptions, function(error, info){
-    //           if (error) {
-    //          console.log(error);
-    //           } else {
-    //             console.log('Email sent: ' + info.response);
-    //           }
-    //         });
-    //     }
-    // } else {
-    //     if (fractals[47][1] == true){
-    //         const mailOptions = {
-    //             from: 'christariccykid55@gmail.com',
-    //             to: 'meliodasdemonk8ng@gmail.com',
-    //             subject: `Lower Fractal formed at ${data[47][1]}`,
-    //             text: 'Trading Signal'
-    //         };
-              
-    //         transporter.sendMail(mailOptions, function(error, info){
-    //           if (error) {
-    //             console.log(error);
-    //           } else {
-    //             console.log('Email sent: ' + info.response);
-    //           }
-    //         });
-    //     }
-    // }
+            transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+             console.log(error);
+              } else {
+                console.log('Email sent: ' + info.response);
+              }
+            });
+        }
+    } else {
+      if (openPrices21[19] < prevMa50[0] && closePrices21[19] > prevMa50[0]){
+        const mailOptions = {
+            from: 'christariccykid55@gmail.com',
+            to: 'meliodasdemonk8ng@gmail.com',
+            subject: `Market Structure broken at ${prevMa50[0]}`,
+            text: 'Trading Signal'
+        };
+          
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+         console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+    }
+    }
+  } catch (error){
+      console.log(error)
+  }
 };
